827aaf7f0b53591a0c6407957df3bc3608eed8da
Alvaro Segovia <alvarosego01@gmail.com>
2025-03-06 06:07:51 -0400

    Refactor login page layout and form handling; enhance structure with reactive forms and improved validation for a better user experience

---
+++ src/app/modules/public/pages/loginPage/login-form.defs.ts
@@ -0,0 +1,55 @@
+
+import { LayoutRow_I } from "@components/forms/interfaces";
+
+export const loginFormDef: LayoutRow_I[] = [
+  {
+    fields: [
+      {
+        typeField: "text",
+        props: {
+          type: 'email',
+          name: "email",
+          label: "Correo electrónico",
+          placeholder: "Escribe tu correo electrónico",
+          classes: 'w-full',
+          validation_rules: [
+            {
+              type: 'required',
+              layer: 'field',
+              message: 'Correo electrónico requerido'
+            },
+            {
+              type: 'email',
+              layer: 'field',
+              message: 'Formato de correo no válido'
+            }
+          ],
+        },
+      },
+      {
+        typeField: 'text',
+        props: {
+          type: 'password',
+          name: "password",
+          label: "Contraseña",
+          classes: 'w-full',
+          placeholder: "Escribe tu contraseña",
+          validation_rules: [
+            {
+              type: 'required',
+              layer: 'field',
+              message: 'Contraseña requerida'
+            },
+                {
+              type: "password",
+              layer: "field",
+              message: 'Formato de contraseña no válido',
+            },
+          ],
+        }
+      },
+
+    ],
+    classes: 'grid-cols-1'
+  }
+]
--- src/app/modules/public/pages/loginPage/loginPage.component.ts
+++ src/app/modules/public/pages/loginPage/loginPage.component.ts
@@ -1,13 +1,20 @@
+import { loginFormDef } from './login-form.defs';
 
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { LayoutGlobalService } from '@services/layoutGlobal.service';
 import { ButtonComponent } from "@components/button/button.component";
 import { Router, RouterLink } from '@angular/router';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { DynamicFormService } from '@components/forms/services/dynamicForm.service';
+import { LayoutRow_I } from '@components/forms/interfaces';
+import { FormLayoutComponent } from '@components/forms/formLayout/formLayout.component';
 
 @Component({
   selector: 'app-login-page',
   imports: [
-    ButtonComponent,
+   ButtonComponent,
+    FormLayoutComponent,
+    ReactiveFormsModule,
     RouterLink
   ],
   templateUrl: './loginPage.component.html',
@@ -15,11 +22,23 @@ import { Router, RouterLink } from '@angular/router';
 })
 export class LoginPageComponent implements OnInit {
 
+
+  form = signal<FormGroup>(new FormGroup({}));
+  formRows = signal<LayoutRow_I[]>([...loginFormDef])
+
   layoutGlobalService = inject(LayoutGlobalService);
 
   constructor(
+    // private fb: FormBuilder,
+    private dynamicFormService: DynamicFormService,
     private router: Router
-  ){
+
+  ) {
+    this.initForm();
+  }
+
+  initForm(){
+    this.form.set(this.dynamicFormService.generateForm(this.formRows()));
 
   }
 
@@ -27,15 +46,29 @@ export class LoginPageComponent implements OnInit {
     this.layoutGlobalService.layoutFullScreen.set(true);
     this.layoutGlobalService.hideNavbar.set(true);
     this.layoutGlobalService.hideFooter.set(true);
+
   }
 
-  emitSubmit(){
+  emitSubmit() {
 
   }
 
   goTo(route: string) {
     this.layoutGlobalService.setLayoutDefault();
     this.router.navigate([route]);
+
+  }
+
+  onSubmit() {
+    this.dynamicFormService.setSubmitted(this.form());
+    if (this.form().valid) {
+      console.log('Formulario enviado:', this.form().value);
+      this.emitSubmit();
+    } else {
+      this.form().updateValueAndValidity();
+      console.log('Formulario inválido');
+    }
+
   }
 
 }
