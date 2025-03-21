a192e562561dbc3a4362d93beb1fe785f069f120
Alvaro Segovia <alvarosego01@gmail.com>
2025-03-21 13:18:06 -0400

    Add DropDownMenu component and integrate into HomePage; remove unused popover scripts

---
+++ src/app/core/components/menus/dropDownMenu/dropDownMenu.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drop-down-menu',
+  imports: [],
+  templateUrl: './dropDownMenu.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DropDownMenuComponent { }
--- src/app/modules/public/pages/homePage/homePage.component.ts
+++ src/app/modules/public/pages/homePage/homePage.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DropDownMenuComponent } from '../../../../core/components/menus/dropDownMenu/dropDownMenu.component';
 
 @Component({
   selector: 'app-home-page',
   imports: [
     // ButtonComponent
+    DropDownMenuComponent
   ],
   templateUrl: './homePage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
