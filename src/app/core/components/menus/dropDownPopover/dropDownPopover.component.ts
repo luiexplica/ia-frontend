181d71c0d9221dc08795b09eb045f990aecee1b3
Alvaro Segovia <alvarosego01@gmail.com>
2025-04-01 17:42:30 -0400

    Refactor dropdown menu components; replace menuList with menu in DropDownPopover, integrate NestedMenu for improved structure, and update userDialog to reflect changes

--- src/app/core/components/menus/dropDownPopover/dropDownPopover.component.ts
+++ src/app/core/components/menus/dropDownPopover/dropDownPopover.component.ts
@@ -1,17 +1,15 @@
-import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { PopoverModule } from 'primeng/popover';
 import { MenuItem_I } from '@interfaces/menus.interface';
-import { ButtonComponent } from '@components/button/button.component';
+import { NestedMenuComponent } from '../nestedMenu/nestedMenu.component';
 
 @Component({
   selector: 'app-drop-down-popover',
   imports: [
     ButtonModule,
     PopoverModule,
-    ButtonComponent,
-    NgClass
+    NestedMenuComponent,
   ],
   templateUrl: './dropDownPopover.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,7 +17,7 @@ import { ButtonComponent } from '@components/button/button.component';
 export class DropDownPopoverComponent {
 
   title = input<string>('');
-  menuList = input<MenuItem_I[]>([
+  menu = input<MenuItem_I[]>([
     // {
     //   title: 'Settings',
     //   active: false,
---
+++ src/app/core/components/menus/nestedMenu/nestedMenu.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MenuItem_I } from '@interfaces/menus.interface';
+import { ButtonComponent } from '@components/button/button.component';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-nested-menu',
+  imports: [
+    ButtonComponent,
+    NgClass
+  ],
+  templateUrl: './nestedMenu.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NestedMenuComponent {
+
+  menu = input<MenuItem_I[]>([]);
+
+}
